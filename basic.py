e5c1f8219b8c94df7e119737255d3c59113d0066
karthikeyan2420 <karthikeyan2420@gmail.com>
2025-11-19 10:07:54 +0530

    Initial commit

---
+++ basic.py
@@ -0,0 +1,116 @@
+# print("first class")
+# print("first","class",sep="*")
+# print("first" , end=" ")
+# print("class")
+
+
+#if else
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+
+# if a>b:
+#     print("a is greater")
+# else:
+#     print("b is greater")
+
+# if a>b:
+#     print("a is greater")
+# elif b>a:
+#     print("b is greater")
+# else:
+#     print("both are equal")
+
+
+
+#loop
+
+# for x in range(5):
+#     print(x)
+
+# for x in range(1,5):
+#     print(x)
+
+# for x in range(1,5,2):
+#     print(x)
+
+# a=5
+# while a>0:
+#     print(a)
+#     a=a-1
+
+
+# a=[45,56,78,98,65,13]
+# print(a[0])
+
+# for x in a:
+#     print(x)
+
+
+#list
+
+# fru=["apple","orange","blueberry","mango","banana","grapes","apple"]
+# print(fru)
+# print(fru[0])
+
+# fru.append("banana")
+# print(fru)
+
+
+# fru.pop()
+# print(fru)
+
+# fru.remove("apple")
+# print(fru)
+
+# fru.sort()
+# print(fru)
+
+# fru.reverse()
+# print(fru)
+
+
+# #tuple
+
+# n=12,56
+# print(n)
+# print(n[0])
+
+# #set
+
+# fru={"apple","orange","blueberry","mango","banana","grapes","apple"}
+# print(fru)
+
+# #dist
+
+# details={"name":"karthi","age":23,"gender":"male"}
+# print(details)
+# print(details["name"])
+# print(details["age"])
+
+
+
+
+#and
+
+a=58
+b=56
+
+print(a>50 and b>50)
+
+
+#or
+
+a=True
+b=False
+
+print(a or b)
+
+
+#not
+
+a=True
+b=False
+
+print(not a)
+print(not b)
